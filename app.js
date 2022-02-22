a2dcd48bf10c96722f6f865fb4afb89aad91ddb9
iMadfxq <imadfxq@gmail.com>
2022-02-22 13:01:27 -0600

    Logic almost finished, UI is being updated when we search

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 
-let form = document.querySelector('form')
-let input = document.querySelector('#input')
+let form = document.querySelector('form');
+let input = document.querySelector('#input');
+let card = document.querySelector('.card');
+let cardDetails = document.querySelector('.card--details');
+
+const updateCity = async (city) => {
+  let cityDetails = await getCity(city);
+  let weather = await getWeather(cityDetails.Key);
+  return {
+    cityDetails,
+    weather
+  };
+};
+
+const updateUI = async (data) => {
+  const {cityDetails, weather} = data
+
+  cardDetails.innerHTML = `
+    <h1>${cityDetails.EnglishName}</h1>
+    <h2>${cityDetails.Country.EnglishName}</h2>
+    <h2>It has a temperature of: ${weather.Temperature.Metric.Value}°C</h2>
+    <h2>${weather.WeatherText}</h2>
+    <p>See more detailed info: <a href="${weather.Link}">Here</a></p>
+    `
+  
+  card.style.display = 'flex'
+}
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
+  e.preventDefault(); //I don't want the page to be reloaded
 
-  let userCity = input.value;
+  let userCity = input.value.trim(); //We get the value of the input, and we are removing any spaces that it has
+  form.reset(); //I want the input to be cleared
 
-  getCity(userCity).then(data => {
-    return getWeather(data.Key);
-  }).then(data => {
-    console.log(data);
-  }).catch(err => console.log(err));
+  updateCity(userCity)
+    .then(data => updateUI(data));
 
 })
